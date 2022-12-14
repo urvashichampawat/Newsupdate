d009b840866d84441286c80491b8b3f6761b2d27
Urvashi Champawat <urvashichampawat@gmail.com>
2022-12-15 02:14:15 +0530

    Before Router

---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" >
+          {/*style={{width: "18rem"}}*/}
+  <img src={!imageUrl?"https://images.indianexpress.com/2022/12/MIUI-14.jpg":imageUrl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href= {newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read more</a>
+  </div>
+</div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
